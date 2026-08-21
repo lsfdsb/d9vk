d7e26cfa24ea5fd5d23cd38b670574010f296faf
Robin Kertels <robin.kertels@gmail.com>
2026-08-22 01:13:35 +0200

    [MTLHACKS][d3d11] Make fllModeNonSolid optional

--- src/d3d11/d3d11_device.cpp
+++ src/d3d11/d3d11_device.cpp
@@ -1976,7 +1976,7 @@ namespace dxvk {
     if (featureLevel >= D3D_FEATURE_LEVEL_9_1) {
       enabled.core.features.depthClamp                            = VK_TRUE;
       enabled.core.features.depthBiasClamp                        = VK_TRUE;
-      enabled.core.features.fillModeNonSolid                      = VK_TRUE;
+      enabled.core.features.fillModeNonSolid                      = supported.core.features.fillModeNonSolid;
       enabled.core.features.pipelineStatisticsQuery               = supported.core.features.pipelineStatisticsQuery;
       enabled.core.features.sampleRateShading                     = VK_TRUE;
       enabled.core.features.samplerAnisotropy                     = supported.core.features.samplerAnisotropy;
